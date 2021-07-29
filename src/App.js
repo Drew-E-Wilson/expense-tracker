58430aea3f715f8786d283d9e85e1d3bface444e
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-07-29 15:33:29 -0400

    starting dropdown list, sass created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expense Tracker</h1>
+      <select name="selectExpense" id="selectExpense">
+        <option value="">Housing</option>
+        <option>Food</option>
+        <option>Transportation</option>
+        <option>Utilities</option>
+        <option>Insurance</option>
+        <option>Savings</option>
+        <option>Personal Spending</option>
+        <option>Other</option>
+      </select>
     </div>
   );
 }
